bdd8bb10a7be03cbfdb9f202867d941640f7e415
Valentin Clement <clementval@gmail.com>
2018-07-19 13:53:31 +0200

    Simplify test data

--- F-FrontEnd/test/testdata/issue66.f90
+++ F-FrontEnd/test/testdata/issue66.f90
@@ -4,14 +4,10 @@ MODULE mod1
   PUBLIC :: netcdf_read_att_int
 
   INTERFACE netcdf_read_att_int
-    MODULE PROCEDURE netcdf_read_ATT_INT
   END INTERFACE netcdf_read_att_int
 
 CONTAINS
-  FUNCTION netcdf_read_ATT_INT(file_id, variable_name, attribute_name) result(res)
+  FUNCTION netcdf_read_att_int() result(res)
     INTEGER                      :: res
-    INTEGER, INTENT(IN)          :: file_id
-    CHARACTER(LEN=*), INTENT(IN) :: variable_name
-    CHARACTER(LEN=*), INTENT(IN) :: attribute_name
-  END FUNCTION netcdf_read_ATT_INT
+  END FUNCTION netcdf_read_att_int
 END MODULE mod1
